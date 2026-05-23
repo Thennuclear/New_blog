8796b23ce1a66c06a3f2892b504fab4c8aa75225
thenuclear <280480017@qq.com>
2026-05-23 21:15:11 +0800

    1

--- src/config.ts
+++ src/config.ts
@@ -20,8 +20,8 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: "Thenuclear's blog",
-	subtitle: "博客大更新完成！",
+	title: "Enucleare's blog",
+	subtitle: "一个摆烂的博客",
 	siteURL: "https://thenuclear.online/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-08-30", // 站点开始运行日期，用于站点统计组件计算运行天数
 
@@ -49,7 +49,7 @@ export const siteConfig: SiteConfig = {
 	// 顶栏标题配置
 	navbarTitle: {
 		// 顶栏标题文本
-		text: "Thenuclear",
+		text: "Enucleare",
 		// 顶栏标题图标路径，默认使用 public/assets/home/home.png
 		icon: "assets/home/home.png",
 	},
@@ -134,11 +134,10 @@ export const siteConfig: SiteConfig = {
 
 		homeText: {
 			enable: true, // 在主页显示自定义文本
-			title: "一个硬套模版的blog", // 主页横幅主标题
+			title: "一个套模版的blog", // 主页横幅主标题
 
 			subtitle: [
-				"大更新总算是完成了",
-				"还有，这个域名是我花了6块钱买的",
+				"这个域名是我花了6块钱买的",
 				"但是只有一年",
 				"之后续费要30美元",
 				"所以以后域名可能会更换",
@@ -294,7 +293,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/touxiang.jpg", // 相对于 /src 目录。如果以 '/' 开头，则相对于 /public 目录
-	name: "Thenuclear",
+	name: "Enucleare",
 	bio: "Never gonna give you up",
 	typewriter: {
 		enable: true, // 启用个人简介打字机效果
